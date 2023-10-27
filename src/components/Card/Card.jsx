91bc998ca65d26dd483ef5bf52def200f4bb2395
supp3rdan <danrleyinformal@gmail.com>
2023-10-27 18:44:54 +0000

    Consumindo tags da API

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,13 @@ import "./Card.css"
 import  Tag  from '../Tag/Tag'
 export default function Card(props) {
     const item = props.item
-    const tags = item.tag || []
+    // const tags = item.tag || []
+    const tags = [
+        `Status: ${item.status}`, 
+        `Species: ${item.species}`, 
+        `Origin: ${item.origin.name}`
+    ]
+    
     return <div className="card">
             <h2>{item.name}</h2>
             <div className="tags">
