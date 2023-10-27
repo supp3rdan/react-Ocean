176e231de5a9aaad7f705c57385e9fc50b079e47
supp3rdan <danrleyinformal@gmail.com>
2023-10-27 01:30:04 +0000

    Criação do component Tag

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,10 @@
 import "./Card.css"
-
+import  Tag  from '../Tag/Tag'
 export default function Card(props) {
     const item = props.item
     return <div className="card">
             <h2>{item.nome}</h2>
-            <div className="tags">
-                <div className="tag">Status: Vivo</div>
-                <div className="tag">Espécie: Humana</div>
-                <div className="tag">Origem: Terra C-137</div>
-            </div>
+            <Tag />
             <img src={item.imagemUrl} alt="Rick Sanchez" />  
         </div>  
     
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,10 @@
+import "./Tag.css"
+
+export default function Tag() {
+    
+    return  <div className="tags">
+                <div className="tag">Status: Vivo</div>
+                <div className="tag">Espécie: Humana</div>
+                <div className="tag">Origem: Terra C-137</div>
+            </div>
+}
