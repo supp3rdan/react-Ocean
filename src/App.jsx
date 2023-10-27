f66582ddc1346fac6173198a776dbb1d8a3af4ab
supp3rdan <danrleyinformal@gmail.com>
2023-10-27 01:04:03 +0000

    Criação od 3 Card e varredura dinamica dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ function App() {
     nome: 'Rick Sanchez',
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+  const item3 = {
+    nome: 'Summer Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+  const itens = [item1, item2, item3]
   return (
     <>
-      <Card item={item1} />
-      
+      {itens.map(function (item) {
+        return <Card item={item} />
+      })}
     </>
   )
 }
