3ca1c4c45061e748daf54d8c288c1d4e91fbe5c8
supp3rdan <danrleyinformal@gmail.com>
2023-10-28 00:07:04 +0000

    Deploy finalizado e arquivos do projeto excluidos

--- src/components/Tag/Tag.jsx
+++
@@ -1,8 +0,0 @@
-import "./Tag.css"
-
-export default function Tag(props) {
-    const tag = props.text
-    return  <div className="tags">
-                <div className="tag">{tag}</div>
-            </div>
-}
